01be5d5d1cf00fe48da30bbfb7be0080b1ead793
Astroakanksha <62433778+Astroakanksha24@users.noreply.github.com>
2022-01-01 21:59:11 +0530

    1-Jan-22

---
+++ GoldmanSachs/Anagrams.cpp
@@ -0,0 +1,55 @@
+
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+
+class Solution{
+  public:
+    vector<vector<string> > Anagrams(vector<string>& string_list) {
+        //code here
+        unordered_map<string,vector<string>> v;
+        for(int i=0;i<string_list.size();i++)
+        {
+            string temp=string_list[i];
+            sort(temp.begin(),temp.end());
+            v[temp].push_back(string_list[i]);
+            
+        }
+        vector<vector<string>> ans;
+        for(auto i:v)
+        {
+           ans.push_back(i.second);
+        }
+        return ans;
+    }
+};
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<string> string_list(n);
+        for (int i = 0; i < n; ++i)
+            cin>>string_list[i]; 
+        Solution ob;
+        vector<vector<string> > result = ob.Anagrams(string_list);
+        sort(result.begin(),result.end());
+        for (int i = 0; i < result.size(); i++)
+        {
+            for(int j=0; j < result[i].size(); j++)
+            {
+                cout<<result[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+
+    return 0;
+}
+ 
